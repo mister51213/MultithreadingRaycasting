e0a71029988179ed636552e2d1b74c674046189e
mister51213 <gregory.weiner@gmail.com>
2018-08-13 20:44:17 +0800

    Added texture mapping. Currently crashing.

--- WinRaycaster/WinRaycaster/World.h
+++ WinRaycaster/WinRaycaster/World.h
@@ -1,30 +1,5 @@
 #pragma once
 
-/*
-struct Cell {
-	// Coordinates start at TOP LEFT CORNER
-	// Positive X goes RIGHT	// Positive Y goes DOWN
-	Vec2 topLeft, bottomRight;
-
-	Cell();
-};
-
-class World {
-private:
-	World();
-	World(const float w, const float h, const Vec2 cellSize);
-	void InitializeMatrix();
-
-	static World* _world;
-	float _worldWidth, _worldHeight;
-	vector<vector<Cell>> _cells;
-	Vec2 _cellSize;
-
-public:
-	static World * GetWorld();
-	static World * GetWorld(const float w, const float h, const Vec2 cellSize);
-};
-*/
 
 class Pixel {
 public:
@@ -89,45 +64,110 @@ public:
 };
 
 
+class Texture {
+public:
+	Pixel	*pImage;
+	int		Wid;
+	int		Hei;
+
+	Texture() :
+		pImage(nullptr), Wid(0), Hei(0) {}
+
+	Texture(const wchar_t *Filename) : Texture() {
+		Bitmap *pBmp = Bitmap::FromFile(Filename);
+		assert(pBmp);
+		
+		BitmapData bd;
+		if (pBmp->LockBits(NULL, ImageLockModeRead, PixelFormat32bppARGB, &bd) != Ok) {
+			int err = GetLastError();
+			return;
+		}
+
+		Wid = bd.Width;
+		Hei = bd.Height;
+
+		pImage = new Pixel[Wid * Hei];
+		assert(pImage);
+
+		const int stride = abs(bd.Stride) / sizeof(Pixel);
+
+		for (int row = 0; row < Hei; row++)
+			memcpy(pImage + row * Wid, (Pixel*)bd.Scan0 + row * stride, Wid * sizeof(Pixel));
+
+		pBmp->UnlockBits(&bd);
+		delete pBmp;
+	}
+};
+
+
+class Cell {
+public:
+	Texture *pWallTex;	// Wall is solid if assigned, Hollow if not
+
+	Cell() :
+		pWallTex(nullptr) {}
+};
+
+
 template <int Size> class Map {
 protected:
-	vector<Pixel>	map;
+	vector<Cell>	map;
 
 public:
 	struct TraceHit {
-		Pixel	*pCell;
-		Vec2	Coord;
-		float	Dist;
+		Cell	*pCell;
+		//float	Dist;
+		Vec2	Pos;
+		int		CellX, LastX;
+		int		CellY, LastY;
 
-		TraceHit(Pixel *pCell, const Vec2 &Coord, const float Dist) :
-			pCell(pCell), Coord(Coord), Dist(Dist) {}
+		TraceHit() : 
+			pCell(nullptr) {}
+
+		TraceHit(Pixel *pCell, const float Dist, const Vec2 &Pos, const int CellX, const int CellY) :
+			pCell(pCell), Dist(Dist), Pos(Coord), CellX(CellX), CellY(CellY) {}
 	};
 
 	int WallHeight;
 
 	Map(const int WallHeight) : WallHeight(WallHeight) {
-		map = vector<Pixel>(Size * Size, 0);
+		map = vector<Cell>(Size * Size);
 	}
 
-	inline Pixel& Cell(const int x, const int y) {
+	inline Cell& GetCell(const int x, const int y) {
 		assert(x >= 0 && x < Size);
 		assert(y >= 0 && y < Size);
 
 		return map[x + y * Size];
 	}
 
+	inline bool InBounds(const Vec2 &Pos) const {
+		return Pos.x >= 0.0f && Pos.x < (float)Size && 
+			   Pos.y >= 0.0f && Pos.y < (float)Size;
+	}
+
 	TraceHit Trace(const Vec2 &Origin, const Vec2 &Step) {
-		const float size = (float)Size;
-		
-		Vec2 pos = Origin;
-		for (; pos.x >= 0.0f && pos.x < size && pos.y >= 0.0f && pos.y < size; pos += Step) {
-			Pixel &cell = map[(int)pos.x + (int)pos.y * Size];
+		TraceHit hit;
+		hit.Pos = Origin;
+
+		hit.LastX = hit.LastY = 0;
+
+		for (; InBounds(hit.Pos); hit.Pos += Step) {
+			hit.CellX = (int)hit.Pos.x;
+			hit.CellY = (int)hit.Pos.y;
+
+			hit.pCell = map.data() + hit.CellX + hit.CellY * Size;
 			
-			if (cell.Alpha())
-				return TraceHit(&cell, pos, (pos - Origin).Size());
+			if (hit.pCell->pWallTex) {
+				//hit.Dist = (hit.Pos - Origin).Size();
+				return hit;
+			}
+
+			hit.LastX = hit.CellX;
+			hit.LastY = hit.CellY;
 		}
 
-		return TraceHit(nullptr, pos, 0);
+		return TraceHit();
 	}
 
 	void Scan(Camera &Cam) {
@@ -152,18 +192,66 @@ public:
 	}
 
 	void Render(Camera &Cam, const TraceHit &Hit, const int Col) {
-		const int hei = (int)((float)WallHeight / (Hit.Dist /* + 1.0f*/));
-		const int half = hei / 2;
+		Texture *pWall = Hit.pCell->pWallTex;
+
+		//Cam.pImage[Col] = 0xFF000000;
+		//Cam.pImage[Col + (Cam.Hei - 1) * Cam.Wid] = 0xFF000000;
+
+		/*const float fracX = Hit.Pos.x - (float)Hit.CellX;
+		const float fracY = Hit.Pos.y - (float)Hit.CellY;
+
+		const int texU = (int)(min(fracX, fracY) * (float)pWall->Wid);*/
+
+		Vec2 path = Hit.Pos - Cam.Pos;
+
+		float fracX = Hit.Pos.x - (float)Hit.CellX;
+		float fracY = Hit.Pos.y - (float)Hit.CellY;
+		float fracU;
+		Vec2 hitPos;
+
+		if (Hit.LastX != Hit.CellX) {
+			// Crossed a vertical boundary
+
+			//const float ofs = path.y / path.x * fracX;
+			//fracU = fracY - ofs;
+			fracU = fracY;
+			
+			//hitPos = Vec2((float)Hit.CellX, (float)Hit.CellY + fracU);
+		} else {
+			// Crossed a horizontal boundary
+
+			//const float ofs = path.x / path.y * fracY;
+			//fracU = fracX - ofs;
+			fracU = fracX;
+
+			//hitPos = Vec2((float)Hit.CellX + fracU, (float)Hit.CellY);
+		}
+
+		hitPos = Hit.Pos;
+
+		const float dist = (hitPos - Cam.Pos).Size();
+
+		const float hei = (float)WallHeight / dist;
+		const int half = (int)(hei / 2.0f);
 		const int top = max(Cam.Hei / 2 - half, 0);
 		const int btm = min(Cam.Hei / 2 + half, Cam.Hei);
-		
-		Cam.pImage[Col] = 0xFF000000;
-		Cam.pImage[Col + (Cam.Hei - 1) * Cam.Wid] = 0xFF000000;
+
+		const float sclV = (float)pWall->Hei / hei;
+
+		const int texU = (int)(fracU * (float)pWall->Wid);
+		assert(texU >= 0 && texU < pWall->Wid);
+
+		Pixel *pTex = pWall->pImage + texU;
+		const int utop = Cam.Hei / 2 - half;
 
 		int ofs = Col + top * Cam.Wid;
 		for (int y = top; y < btm; y++, ofs += Cam.Wid) {
 			//Cam.pImage[Col + y * Cam.Wid] = *Hit.pCell;
-			Cam.pImage[ofs] = *Hit.pCell;
+			//Cam.pImage[ofs] = *Hit.pCell;
+			const int texV = (int)((float)(y - utop) * sclV);
+			assert(texV >= 0 && texV < pWall->Hei);
+
+			Cam.pImage[ofs] = pTex[texV * pWall->Wid];
 		}
 	}
 };
